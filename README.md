e39a164a446164e5086caa790d575c53406b94a2
raphsenn <raphaelsenn@outlook.de>
2024-01-06 13:31:17 +0100

    readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # some-optimization-algorithms
-Just some optimization algorithms
+
+## Algorithms
+- Gradient descent
+- Gauss-Newton algorithm
+- Conjugate gradient method
+
+### Gradient descent
+Gradient descent is an iterativ Algorithm to find a local optimum (minimum) of a differentiable multivariate function.
--- gradient-descent/sin_x_cos_y.py
+++ gradient-descent/sin_x_cos_y.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from matplotlib import pyplot as plt
 
 
@@ -9,18 +9,33 @@ def f(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     return np.sin(x) + np.cos(y)
 
 
-def f_grad(x: np.ndarray, y: np.ndarray) -> np.ndarray, np.ndarray:
-    """ 
+def f_grad(x: np.ndarray, y: np.ndarray) -> tuple:
+    """
     Represents f: R^2 -> R^2 with f'(x, y) = (cos(x), -sin(x))
-    """ 
+    """
     grad_x = np.cos(x)
     grad_y = -np.sin(y)
     return grad_x, grad_y
 
 
-def optimizer_with_backtracking(x_0: float, beta: float, max_iterations: int, plot: bool):
-    pass
+def optimize_with_backtracking(x_0: float, y_0: float, beta: float, max_iterations: int, plot: bool):
+    # Data for plotting. 
+    x = np.linspace(-1, 1, 1000)
+    y = np.linspace(-1, 1, 1000)
+    
+    # Create a meshgrid.
+    X,Y = np.meshgrid(x,y)
+    Z = f(X, Y)
+
+    if plot:
+        # Plotting setup
+        plt.figure(num="f(x, y) = sin(x) + cos(y)") 
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.contour(X, Y, Z, cmap='viridis') 
+        plt.colorbar(label='Function Value')    
+        plt.show()
 
 
 if __name__ == '__main__':
-    pass
+    optimize_with_backtracking(1, 1, 0.08, 100, True)
